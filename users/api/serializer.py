567d391a0d8712ae41258b3811cc30d321e80a88
asiernc <asierncara@gmail.com>
2025-03-11 12:23:54 +0100

    done donedone

--- users/api/serializer.py
+++ users/api/serializer.py
@@ -9,7 +9,7 @@ class UserSerializer(serializers.ModelSerializer):
 	
 	class Meta:
 		model = User
-		fields = [ 'username', 'email', 'password', 'avatar_field', 'otp', 'otp_expire', 'online_status' ]
+		fields = [ 'username', 'email', 'password', 'avatar_field', 'avatar_42_url', 'otp', 'otp_expire', 'online_status' ]
 		extra_kwargs = {
 			'password': {'write_only': True, 'required': False},
 		}
